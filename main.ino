87bf57022e04482cf4876758806d4e62da1e4075
Biohazard86 <52355093+Biohazard86@users.noreply.github.com>
2021-04-19 15:06:32 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,34 +1,56 @@
+
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
 unsigned int pin_inj_1 = 2; // pin 2 to the injector n1
 unsigned int led_1 = 1;      // LED for the injector pulse
+const int analogPin = A0; 
+
+// i2c LCD
+LiquidCrystal_I2C lcd(0x3F,16,2);  //
+
+// ------------------------------------------------------------------
 
 void setup() {
- pinMode(pin_inj_1, OUTPUT);    // pin 2 as an output
- digitalWrite(pin_inj_1, LOW);
+  Serial.begin(9600); // open the serial port at 9600 bps:
+  
+  pinMode(pin_inj_1, OUTPUT);    // pin 2 as an output
+  digitalWrite(pin_inj_1, LOW);
 
- pinMode(led_1, OUTPUT);
- digitalWrite(led_1, LOW);
+  pinMode(led_1, OUTPUT);
+  digitalWrite(led_1, LOW);
 
- 
 
 }
 
+// ------------------------------------------------------------------
+
 void do_pulse(int ms_pulse, int pin){
 
-  unsigned int micros1, micros2;
-  
+  unsigned int micros1, micros_pulse;
 
+  micros_pulse = ms_pulse*1000; // When the end of the pulse has to happen in microseconds
+
+  // Put on the injector
   pinMode(pin, HIGH);
+  // Read the micros of the MCU
   micros1 = micros();
+  
   do{
-    
-    }while(micros() < (micros1 + ) )
+    // dont do anything until the pulse duration passed
+  }while( micros() < (micros1+micros_pulse ) );
+  //Then turn off the injector
   pinMode(pin, LOW);
   
   }
 
 
-
+// ------------------------------------------------------------------
 void loop() {
-  // put your main code here, to run repeatedly:
+  int selector_value, hz_value=0, duty_cyle=0, rpm_engine;
+
+
+  
 
 }
